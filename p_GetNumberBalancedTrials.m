4c2d14ecc6783c605e237cd859832364de05b9f0
David Baum <github@davidb.anonaddy.com>
2022-01-29 23:04:10 -0300

    More info about rejection reason

--- p_GetNumberBalancedTrials.m
+++ p_GetNumberBalancedTrials.m
@@ -33,8 +33,12 @@ load(file_AllEvents);
                   
 
 TotalTrials     = zeros(size(AllEvents, 2), 1);
-ValidTrials     = zeros(size(AllEvents, 2), 1);
-VerbalOutput    = cell(size(AllEvents, 2), 3);
+RjectTrials     = zeros(size(AllEvents, 2), 1);
+VerbalOutput    = cell(size(AllEvents, 2), 4);
+VariablesOut    = {'Dataset', 'Total (#)', 'Rejected (%)', 'Rejected (#)', ...
+    'Incomplete cycles (#)', 'Complete cycles (#)', ...
+    'Rej. bc artifact (Cycles, #)', 'Rej. bc short (Cycles, #)', ...
+    'Valid trials (#)'};
 
 for iSubj = 1:size(AllEvents, 2)
     
@@ -53,11 +57,10 @@ for iSubj = 1:size(AllEvents, 2)
     
     NumberTrials            = min([numel(IdxTrialsOdor), numel(IdxTrialsVehicle)]);
     
-
-    IdxBadTrial             = find(~strcmp(Reasons, ''));
-    IdxBadBecauseOff        = find(ismember(IdxBadTrial, IdxTrialsOff));
-    IdxBadTrial(IdxBadBecauseOff) = []; % Not actually bad trials, but OFF
-
+    IdxBadBecauseShort      = find(strcmp(Reasons, 'too short'));
+    IdxBadBecauseArtifact   = find(strcmp(Reasons, 'artifact'));
+    IdxBadTrial             = sort([IdxBadBecauseShort, IdxBadBecauseArtifact]);
+%     IdxBadTrial             = sort([IdxBadBecauseArtifact]);
     
     % Build complete cycles matrix
     CompleteCycles          = [];
@@ -70,23 +73,44 @@ for iSubj = 1:size(AllEvents, 2)
         end
     end
     
-    RejectedCycles          = 0;
+    RejectedIncomplete = 0;
+    RejectedComplete   = 0;
+    RejectedShort      = 0;
+    RejectedArtifact   = 0;
     for iCycle = 1:size(CompleteCycles, 1)
         if any(ismember(IdxBadTrial, CompleteCycles(iCycle, :))) % Any
-            RejectedCycles = RejectedCycles +1;
+            if sum(ismember(IdxBadTrial, CompleteCycles(iCycle, :))) > 1
+                RejectedComplete = RejectedComplete + 1;
+            else
+                RejectedIncomplete = RejectedIncomplete + 1;
+            end
+            
+            if any(ismember(IdxBadBecauseShort, CompleteCycles(iCycle, :)))
+                RejectedShort = RejectedShort + 1;
+            end
+            if any(ismember(IdxBadBecauseArtifact, CompleteCycles(iCycle, :)))
+                RejectedArtifact = RejectedArtifact + 1;
+            end
+            
         end
     end
+    RejectedCycles = RejectedIncomplete + RejectedComplete;
     
     % Subject's information
     TotalTrials(iSubj)      = NumberTrials;
-    ValidTrials(iSubj)      = RejectedCycles;
+    RjectTrials(iSubj)      = RejectedCycles;
+    
     
     VerbalOutput{iSubj, 1}  = AllEvents{1, iSubj};
-    VerbalOutput{iSubj, 2}  = strcat(string(RejectedCycles), '/', ...
-        string(NumberTrials));
+    VerbalOutput{iSubj, 2}  = NumberTrials;
     VerbalOutput{iSubj, 3}  = ceil(RejectedCycles * 100 / NumberTrials);
+    VerbalOutput{iSubj, 4}  = RejectedCycles;
+    VerbalOutput{iSubj, 5}  = RejectedIncomplete;
+    VerbalOutput{iSubj, 6}  = RejectedComplete;
+    VerbalOutput{iSubj, 7}  = RejectedArtifact;
+    VerbalOutput{iSubj, 8}  = RejectedShort;
+    VerbalOutput{iSubj, 9}  = NumberTrials - RejectedCycles;
 
 end
 
-ToPrint = cell2table(VerbalOutput, ...
-    'VariableNames', {'Dataset', 'Rejected/Total', 'Rejected (%)'})
+ToPrint = cell2table(VerbalOutput, 'VariableNames', VariablesOut)
